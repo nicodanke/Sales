b49351f86eef89c0ddc084100192e112ab49a613
Nicolas Dankiewicz <nicodanki@hotmail.com>
2018-08-18 17:19:22 -0300

    Part 6

---
+++ Sales.Backend/Models/LocalDataContext.cs
@@ -0,0 +1,13 @@
+﻿namespace Sales.Backend.Models
+{
+    using Domain.Models;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class LocalDataContext : DataContext
+    {
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
+    }
+}
