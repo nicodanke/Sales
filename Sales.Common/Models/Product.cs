52e554adc6114b524d2c9a58989f88917a861a08
Nicolas Dankiewicz <nicodanki@hotmail.com>
2018-08-19 00:38:19 -0300

    Part 7.1

--- Sales.Common/Models/Product.cs
+++ Sales.Common/Models/Product.cs
@@ -23,5 +23,9 @@ namespace Sales.Common.Models
 
         public DateTime PublishOn { get; set; }
 
+        public override string ToString()
+        {
+            return this.Description;
+        }
     }
 }
